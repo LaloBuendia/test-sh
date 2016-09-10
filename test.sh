aba582102c9db466f821b4e7de5ac002f093799d
LaloBuendia <ingbuendia3@gmail.com>
2016-09-10 18:32:17 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-echo "hola mundo"
 sudo apt-get update
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
